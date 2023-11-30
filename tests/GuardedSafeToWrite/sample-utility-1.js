d4c477a0edcc5e51ce5ea2a46274b42b97d55b6f
Michael Hohn <hohn@github.com>
2023-11-30 14:00:27 -0800

    Add GuardedSafeToWrite test

---
+++ tests/GuardedSafeToWrite/sample-utility-1.js
@@ -0,0 +1,17 @@
+var SampleUtility = function() {
+    var value = this.getParameter('value');
+
+    var ua = new GR('users');
+    ua.query();
+    
+    if(!ua.hasNext()){
+	ua.initialize();
+	ua.setValue('status',value);
+	ua.insert();
+    }
+    else {
+	ua.next();
+	ua.setValue('status',value);
+	ua.update();
+    }
+}
